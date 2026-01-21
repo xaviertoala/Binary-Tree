b8484b8816d333e2e4bf4821778912276719a9da
xaviertoala <xaviertoala2006@gmail.com>
2026-01-20 21:15:53 -0500

    Arbol binario: cambios finales

--- src/main/java/com/espoch/treeimplementation/app/Launcher.java
+++ src/main/java/com/espoch/treeimplementation/app/Launcher.java
@@ -2,6 +2,9 @@ package com.espoch.treeimplementation.app;
 
 import javafx.application.Application;
 
+
+//CLASE PRINCIPAL
+
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(ArbolApp.class, args);
